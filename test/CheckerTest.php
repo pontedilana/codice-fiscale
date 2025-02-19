06b32e6301705d3cddd56953a8b48c364303dda3
Manuel Dalla Lana <manuel@pontedilana.it>
2025-02-19 10:54:58 +0100

    Add tests for omocodie

--- test/CheckerTest.php
+++ test/CheckerTest.php
@@ -3,21 +3,39 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 class CheckerTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @var string[]
+     */
     protected $codiciFiscaliOk;
+
+    /**
+     * @var string[]
+     */
     protected $codiciFiscaliKo;
+
+    /**
+     * @var string[]
+     */
+    protected $omocodie;
 
     public function setUp(): void
     {
         $this->codiciFiscaliOk = [
             "VRDGPP13R10B293P",
             "CHRVRD74S53L219F",
-            "VRDGPP13R10B29PL"
+            "VRDGPP13R10B29PL",
         ];
 
         $this->codiciFiscaliKo = [
             "SLLNDR91C06F205",
             "SXLNDQ67CS8Z210L",
-            "XSD91S67CS8Z210L"
+            "XSD91S67CS8Z210L",
+        ];
+
+        $this->omocodie = [
+            'BNZVCN32S10E57PV',
+            'BNZVCNPNSMLERTPX',
+            'CCHGNN67R05H1S3I',
         ];
     }
 
@@ -31,6 +49,10 @@ class CheckerTest extends \PHPUnit\Framework\TestCase
 
         foreach ($this->codiciFiscaliKo as $cf) {
             $this->assertFalse($checker->isFormallyCorrect($cf));
+        }
+
+        foreach ($this->omocodie as $cf) {
+            $this->assertTrue($checker->isFormallyCorrect($cf));
         }
     }
 }
